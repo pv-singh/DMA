262075764ef7280f43a43dbbc4dd1f5b8565777c
Amit <root@Console>
2017-01-27 14:58:56 +0530

    Consolidated sample applications

--- Sudoku/src/net/jankenpoi/i18n/LocaleListener.java
+++
@@ -1,9 +0,0 @@
-package net.jankenpoi.i18n;
-
-import java.util.Locale;
-
-public interface LocaleListener {
-	
-	void onLocaleChanged(Locale newLocale);
-	
-}
--- Sudoku/src/net/jankenpoi/sudokuki/ui/L10nComponent.java
+++
@@ -1,9 +0,0 @@
-package net.jankenpoi.sudokuki.ui;
-
-import java.util.Locale;
-
-public interface L10nComponent {
-
-	void setL10nMessages(Locale locale, String languageCode);
-	
-}
